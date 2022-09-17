9467cfc780894a89b25f32a90e3515ab31b4c61b
Md Shariful Islam <94960764+shariful94@users.noreply.github.com>
2022-09-17 16:23:37 +0600

    crud complete at a stretch

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 9 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="container">
+
+
+    <h1 class="text-center">welcome to Crud</h1>
+    @yield('content')
+</div>
+    
+</body>
+</html>
